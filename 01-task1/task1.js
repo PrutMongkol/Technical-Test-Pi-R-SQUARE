80a95c965dab638ed47f6cfbe1b5f447790f1090
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-29 12:31:53 +0700

    task1: add Sum Of Two version 1

---
+++ 01-task1/task1.js
@@ -0,0 +1,24 @@
+/*
+assumptions:
+ - 'array' is an array of N integers
+ - 0 <= N < 1e9
+ - 'target' is an integer
+*/
+
+/*
+straight-forward version (unoptimized)
+*/
+function isSumOfTwoExist(array, target) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (i === j) {
+                // no-op
+            } else {
+                if (array[i] + array[j] === target) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
