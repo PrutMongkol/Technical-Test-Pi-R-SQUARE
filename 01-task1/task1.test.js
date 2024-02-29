13b16265d36c33802bc8548e0e7d5da297230c31
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-29 14:14:50 +0700

    task1: add a big array test case

--- 01-task1/task1.test.js
+++ 01-task1/task1.test.js
@@ -1,18 +1,28 @@
-const isSumOfTwoExist = require("./task1");
-
-// test('Array: [1, 3, 5, 7, 16, 4]; Target: 8; Return: true', () => {
-//     expect(isSumOfTwoExist([1, 3, 5, 7, 16, 4], 8)).toBe(true);
-// });
+const { isSumOfTwoExist } = require("./task1.js");
 
 test.each`
-    array | target | expected | type
-    ${[1, 3, 5, 7, 16, 4]} | ${8} | ${true} | ${'standard'}
-    ${[1, 3, 5, 7, 16, 4]} | ${12} | ${true} | ${'standard'}
-    ${[1, 3, 5, 7, 16, 4]} | ${18} | ${false} | ${'standard'}
-    ${[12, 17, 14, 11, 19, 8]} | ${20} | ${true} | ${'standard'}
-    ${[]} | ${1} | ${false} | ${'empty array'}
-    ${[1]} | ${1} | ${false} | ${'single-element'}
-    ${[1,1]} | ${1} | ${false} | ${'no pair'}
-`("Type: $type - array: $array; target: $target; expected return: $expected", ({array, target, expected}) => {
-    expect(isSumOfTwoExist(array, target)).toBe(expected);
-})
+    array                      | target | expected | type
+    ${[1, 3, 5, 7, 16, 4]}     | ${8}   | ${true}  | ${"standard"}
+    ${[1, 3, 5, 7, 16, 4]}     | ${12}  | ${true}  | ${"standard"}
+    ${[1, 3, 5, 7, 16, 4]}     | ${18}  | ${false} | ${"standard"}
+    ${[12, 17, 14, 11, 19, 8]} | ${20}  | ${true}  | ${"standard"}
+    ${[1, 3, 5, -10, 15]}      | ${5}   | ${true}  | ${"standard"}
+    ${[]}                      | ${1}   | ${false} | ${"empty array"}
+    ${[1]}                     | ${1}   | ${false} | ${"single-element"}
+    ${[1, 1]}                  | ${1}   | ${false} | ${"no pair"}
+`(
+    "Type: $type - array: $array; target: $target; expected return: $expected",
+    ({ array, target, expected }) => {
+        expect(isSumOfTwoExist(array, target)).toBe(expected);
+    }
+);
+
+const length = 100000;
+const bigArray = new Array(length).fill(0);
+bigArray[length - 1] = 9;
+bigArray[length - 2] = 1;
+const bigArrayTarget = 10;
+
+test("Type: big array - array: [ 0, ..., 0, 1, 9]; target: 10; expected return: true", () => {
+    expect(isSumOfTwoExist(bigArray, bigArrayTarget)).toBe(true);
+});
