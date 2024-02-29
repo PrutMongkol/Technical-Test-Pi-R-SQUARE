82a18b470fde3c9c66379bfab7f574c56359c876
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-02-29 20:57:27 +0700

    task2: fix incorrect test matcher

--- 02-task2/task2.test.js
+++ 02-task2/task2.test.js
@@ -4,6 +4,6 @@ test.each`
     input                                  | output
     ${[4, 2, 5, 7, 1, 6]}                  | ${[1, 5, 7, 2, 4, 6]}
     ${[25, 40, 14, 91, 31, 22, 49, 13, 6]} | ${[13, 25, 31, 49, 91, 6, 14, 22, 40]}
-`("Input: $input; Output: $output", (input, output) => {
-    expect(customSort(input)).toBe(output);
+`("Input: $input; Output: $output", ({ input, output }) => {
+    expect(customSort(input)).toEqual(output);
 });
