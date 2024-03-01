c6d1d45cee1d6319321fa0337dbb3de5006a3990
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-03-01 12:26:20 +0700

    task3: implement a solution

--- 03-task3/task3.js
+++ 03-task3/task3.js
@@ -1,5 +1,28 @@
-function stringifyLargestNumber(array) {
-    throw new Error("Not Implemented");
+function stringifyLargestNumber(integers) {
+    const numberBucket = [];
+
+    for (let i = 0; i < integers.length; i++) {
+        const number = integers[i];
+        const bucketIndex = number.toString().length;
+        if (!numberBucket[bucketIndex]) {
+            numberBucket[bucketIndex] = [];
+        }
+        numberBucket[bucketIndex].push(number);
+    }
+    
+    let result = "";
+
+    for (let i = 1; i < numberBucket.length; i++) {
+        if (numberBucket[i]) {
+            numberBucket[i].sort();
+            while (numberBucket[i].length > 0) {
+                const number = numberBucket[i].pop();
+                result = result.concat(number);
+            }
+        }
+    }
+
+    return result;
 }
 
 module.exports = stringifyLargestNumber;
