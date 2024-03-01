34e894c15027f1f14480c2b43c7a8d0d05787410
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-03-01 13:06:43 +0700

    task3: add more test cases

--- 03-task3/task3.test.js
+++ 03-task3/task3.test.js
@@ -4,6 +4,9 @@ test.each`
     input | output
     ${[10, 2, 13, 7]} | ${"721310"}
     ${[21, 36, 8, 45]} | ${"8453621"}
+    ${[]} | ${""}
+    ${[0]} | ${"0"}
+    ${[100000, 90, 8, 356, 350]} | ${"890356350100000"}
 `("Input: $input; Output: $output", ({ input, output }) => {
     expect(stringifyLargestNumber(input)).toBe(output);
 });
