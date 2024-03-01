17da9e8ae886666266b8d35d557d46a8802db585
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-03-01 14:04:48 +0700

    task4: add more test cases

--- 04-task4/task4.test.js
+++ 04-task4/task4.test.js
@@ -4,6 +4,12 @@ test.each`
     input | output
     ${10} | ${17}
     ${59} | ${440}
+    ${1}  | ${0}   // Edge case: input is 1
+    ${2}  | ${2}   // Edge case: input is 2
+    ${0}  | ${0}   // Edge case: input is 0
+    ${-5} | ${0}   // Edge case: input is negative
+    ${100} | ${1060}
+    ${999} | ${76127}
 `("Input: $input; Output; $output", ({ input, output }) => {
     expect(sumOfAllPrimes(input)).toBe(output);
 });
