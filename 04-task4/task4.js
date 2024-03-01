319e96140a3fd9d7a220cd4b85c53305dc2c017a
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-03-01 13:48:45 +0700

    task4: implement a solution

--- 04-task4/task4.js
+++ 04-task4/task4.js
@@ -1,5 +1,31 @@
 function sumOfAllPrimes(n) {
-    throw new Error("Not Implemented");
+    if (n < 2) {
+        return 0;
+    }
+
+    const primes = new Array(n + 1).fill(true);
+    primes[0] = false;
+    primes[1] = false;
+
+    let result = 0;
+
+    for (let i = 2; i * i < n; i++) {
+        if (primes[i]) {
+            j = i * i;
+            while (j <= n) {
+                primes[j] = false;
+                j += i;
+            }
+        }
+    }
+
+    for (let i = 2; i < primes.length; i++) {
+        if (primes[i]) {
+            result += i;
+        }
+    }
+
+    return result;
 }
 
 module.exports = sumOfAllPrimes;
